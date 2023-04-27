e89c55eae28c8560ad0bf6765eeeb9705c89d663
akashi7 <christiannseko@gmail.com>
2023-04-27 14:30:10 +0200

    project setup

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,12 @@
 import '@/styles/globals.css'
+import 'tailwindcss/tailwind.css'
 import type { AppProps } from 'next/app'
+import { StyleProvider } from '@ant-design/cssinjs'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <StyleProvider hashPriority='high'>
+      <Component {...pageProps} />
+    </StyleProvider>
+  )
 }
